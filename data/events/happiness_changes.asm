21cb327e3d8281b12d10615c2154bc081b0fcf65
Bruhcation <159866527+Bruhcation@users.noreply.github.com>
2025-01-30 15:50:33 -0800

    happiness

--- data/events/happiness_changes.asm
+++ data/events/happiness_changes.asm
@@ -1,10 +1,10 @@
 HappinessChanges:
 ; entries correspond to HAPPINESS_* constants
 ; change if happiness < 100, change if happiness < 200, change otherwise
-	db  +5,  +3,  +2 ; Gained a level
-	db  +5,  +3,  +2 ; Vitamin
+	db  +10, +6,  +4 ; Gained a level
+	db  +10, +6,  +4 ; Vitamin
 	db  +1,  +1,  +0 ; X Item
-	db  +3,  +2,  +1 ; Battled a Gym Leader
+	db  +6,  +4,  +21 ; Battled a Gym Leader
 	db  +1,  +1,  +0 ; Learned a move
 	db  -1,  -1,  -1 ; Lost to an enemy
 	db  -5,  -5, -10 ; Fainted due to poison
@@ -18,5 +18,5 @@ HappinessChanges:
 	db  -5,  -5, -10 ; Used Heal Powder or Energypowder (bitter)
 	db -10, -10, -15 ; Used Energy Root (bitter)
 	db -15, -15, -20 ; Used Revival Herb (bitter)
-	db  +3,  +3,  +1 ; Grooming
-	db +10,  +6,  +4 ; Gained a level in the place where it was caught
+	db  +6,  +6,  +31 ; Grooming
+	db +20,  +12, +8 ; Gained a level in the place where it was caught
